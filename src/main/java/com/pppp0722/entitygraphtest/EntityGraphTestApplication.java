8d9d93d8e59cfbc0815bbd2cb59e3c605bea750c
ilhwan <poj0722@naver.com>
2022-10-23 18:09:12 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/pppp0722/entitygraphtest/EntityGraphTestApplication.java
@@ -0,0 +1,13 @@
+package com.pppp0722.entitygraphtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityGraphTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityGraphTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pppp0722/entitygraphtest/EntityGraphTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pppp0722.entitygraphtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityGraphTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
